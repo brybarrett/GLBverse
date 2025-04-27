ffcc971d5b2b1f46281dffa78a7834e2e050183e
Logeshvaran <logeshvaran.9876@gmail.com>
2025-04-27 07:31:17 +0530

    messs

--- server/index.js
+++ server/index.js
@@ -9,16 +9,7 @@ const app = express();
 
 // CORS config (allow local + live frontend)
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://server-gbl.vercel.app",
-       "https://client-gbl.vercel.app"
-    ],
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.json({ limit: "50mb" }));
